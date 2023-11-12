5760c7d386f700b95a9d7aa235efbfd5fa8291c4
花花 <73741466+xLikeWATCHDOG@users.noreply.github.com>
2023-11-12 12:23:47 +0800

    Realize the picture preview function.

--- src/main/java/cc/vastsea/toyou/controler/PictureController.java
+++ src/main/java/cc/vastsea/toyou/controler/PictureController.java
@@ -3,6 +3,7 @@ package cc.vastsea.toyou.controler;
 import cc.vastsea.toyou.common.StatusCode;
 import cc.vastsea.toyou.exception.BusinessException;
 import cc.vastsea.toyou.model.dto.PictureMetaGetResponse;
+import cc.vastsea.toyou.model.dto.PicturePreviewRequest;
 import cc.vastsea.toyou.model.dto.SharePictureRequest;
 import cc.vastsea.toyou.model.dto.UserPictureListRequest;
 import cc.vastsea.toyou.model.entity.Picture;
@@ -25,6 +26,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeanUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,6 +42,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 @RestController
 @RequestMapping("/picture")
@@ -162,7 +166,7 @@ public class PictureController {
 		long id = share.getId();
 		UserPicture userPicture = userPictureService.getById(id);
 		if (userPicture == null) {
-			throw new BusinessException(StatusCode.FORBIDDEN, "无权操作");
+			throw new BusinessException(StatusCode.FORBIDDEN, "未找到该图片");
 		}
 		String pid = userPicture.getPid();
 		String fileName = userPicture.getFileName();
@@ -193,7 +197,7 @@ public class PictureController {
 			//将流读到字节数组中
 			is.read(bytes);
 			//设置响应头信息，Content-Disposition响应头表示收到的数据怎么处理（固定），attachment表示下载使用（固定），filename指定下载的文件名（下载时会在客户端显示该名字）
-			response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
+			response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + fileName);
 			//创建输出流
 			OutputStream out = response.getOutputStream();
 			out.write(bytes);
@@ -208,5 +212,43 @@ public class PictureController {
 		userPictureService.setDownloads(id, userPicture.getDownloads() + 1);
 		shareService.setDownloads(uuid, share.getDownloads() + 1);
 		return new ResponseEntity<>("success", null, StatusCode.OK);
+	}
+
+	@GetMapping("/preview")
+	public ResponseEntity<InputStreamResource> previewPicture(PicturePreviewRequest picturePreviewRequest, HttpServletRequest request, HttpServletResponse response) {
+		User user = userService.getLoginUser(request);
+		long uid = user.getUid();
+		long id = picturePreviewRequest.getId();
+		UserPicture userPicture = userPictureService.getUserPicture(uid, id);
+		if (userPicture == null) {
+			throw new BusinessException(StatusCode.FORBIDDEN, "未找到该图片");
+		}
+		String pid = userPicture.getPid();
+		String fileName = userPicture.getFileName();
+		ShareMode shareMode = ShareMode.of(picturePreviewRequest.getShareMode());
+		Picture picture = pictureService.getById(pid);
+		File pictureFile;
+		switch (shareMode) {
+			case PUBLIC -> {
+				pictureFile = new File(picture.getWatermark());
+			}
+			case PUBLIC_COMPRESS -> {
+				pictureFile = new File(picture.getThumbnail());
+			}
+			default -> {
+				pictureFile = new File(picture.getOriginal());
+			}
+		}
+		try {
+			InputStream is = new FileInputStream(pictureFile);
+			HttpHeaders headers = new HttpHeaders();
+			headers.add(HttpHeaders.CONTENT_TYPE, Files.probeContentType(pictureFile.toPath()));
+			headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=" + pictureFile.getName());
+			InputStreamResource inputStreamResource = new InputStreamResource(is);
+			return new ResponseEntity<>(inputStreamResource, headers, StatusCode.OK);
+		} catch (Throwable e) {
+			log.error("预览系统异常", e);
+			throw new BusinessException(StatusCode.INTERNAL_SERVER_ERROR, "预览系统异常");
+		}
 	}
 }
