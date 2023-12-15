2a1c2c7607a24055165f2271e084088635411646
zrll_ <zhengruilin0@126.com>
2023-12-15 23:29:19 +0800

    opt import

--- src/main/java/cc/vastsea/toyou/model/vo/ShareVO.java
+++ src/main/java/cc/vastsea/toyou/model/vo/ShareVO.java
@@ -3,11 +3,9 @@ package cc.vastsea.toyou.model.vo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Date;
 
 public class ShareVO implements Serializable {
     @Serial
