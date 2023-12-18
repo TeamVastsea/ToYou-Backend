e8786950fe7306d6d29c72093cd5691a4bce4750
zrll_ <zhengruilin0@126.com>
2023-12-18 10:39:44 +0800

    add picture share

--- src/main/java/cc/vastsea/toyou/model/vo/ShareVO.java
+++ src/main/java/cc/vastsea/toyou/model/vo/ShareVO.java
@@ -3,10 +3,12 @@ package cc.vastsea.toyou.model.vo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 
+@Data
 public class ShareVO implements Serializable {
     @Serial
     @TableField(exist = false)
