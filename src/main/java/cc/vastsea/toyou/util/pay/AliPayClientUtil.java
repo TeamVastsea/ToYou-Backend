af5183834e64e041898b824fecbd70668ed043ad
M397749490 <397749490@qq.com>
2024-01-10 23:02:49 +0800

    Update AliPayClientUtil.java

--- src/main/java/cc/vastsea/toyou/util/pay/AliPayClientUtil.java
+++ src/main/java/cc/vastsea/toyou/util/pay/AliPayClientUtil.java
@@ -3,8 +3,6 @@ package cc.vastsea.toyou.util.pay;
 import com.alipay.api.*;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Slf4j
@@ -13,29 +11,24 @@ public class AliPayClientUtil {
     private String privateKey;
     private String appCert;
     private String alipayCert;
-
     private String alipayRootCert;
 
     private final AlipayClient alipayClient;
 
     private CertAlipayRequest alipayConfig(String url){
 
-        try {
-            this.privateKey = new String(Files.readAllBytes(Paths.get("./profile/alipay/privateKey.txt")));
-            this.appCert = Paths.get("./profile/alipay/appCertPublicKey_2021004125628045.crt").toString();
-            this.alipayCert  = Paths.get("./profile/alipay/alipayCertPublicKey_RSA2.crt").toString();
-            this.alipayRootCert  = Paths.get("./profile/alipay/alipayRootCert.crt").toString();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        this.privateKey = Paths.get("./profile/alipay/privateKey.txt").toString();
+        this.appCert = Paths.get("./profile/alipay/appCertPublicKey.crt").toString();
+        this.alipayCert  = Paths.get("./profile/alipay/alipayCertPublicKey.crt").toString();
+        this.alipayRootCert  = Paths.get("./profile/alipay/alipayRootCert.crt").toString();
 
         CertAlipayRequest certAlipayRequest = new CertAlipayRequest();
         certAlipayRequest.setServerUrl(url);
         certAlipayRequest.setAppId("2021004125628045");
-        certAlipayRequest.setPrivateKey(privateKey);
         certAlipayRequest.setFormat("JSON");
         certAlipayRequest.setCharset("utf-8");
         certAlipayRequest.setSignType("RSA2");
+        certAlipayRequest.setPrivateKey(privateKey);
         certAlipayRequest.setCertPath(appCert);
         certAlipayRequest.setAlipayPublicCertPath(alipayCert);
         certAlipayRequest.setRootCertPath(alipayRootCert);
