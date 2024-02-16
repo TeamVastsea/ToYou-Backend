1f5e0f8556db010394331b1a38310e623c6b15ae
zrll_ <zhengruilin0@126.com>
2024-02-16 22:46:42 +0800

    add start server

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,45 @@
 use std::fs::OpenOptions;
 use std::io::{Read, Write};
 
+use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use serde_inline_default::serde_inline_default;
+use tokio::fs;
 
+#[serde_inline_default]
 #[derive(Serialize, Deserialize)]
 pub struct Config {
     #[serde(default = "generate_connection_setting")]
     pub connection: ConnectionConfig,
+    #[serde_inline_default(String::from("info"))]
+    pub trace_level: String,
 }
 
 #[serde_inline_default]
 #[derive(Serialize, Deserialize)]
 pub struct ConnectionConfig {
     #[serde_inline_default(String::from("0.0.0.0:7890"))]
-    pub server_uri: String,
+    pub server_addr: String,
     #[serde_inline_default(String::from("postgresql://root:root@127.0.0.1:5432/toyou"))]
     pub db_uri: String,
+    #[serde_inline_default(false)]
+    pub tls: bool,
+    #[serde_inline_default(String::from("./cert.crt"))]
+    pub ssl_cert: String,
+    #[serde_inline_default(String::from("./private.key"))]
+    pub ssl_key: String,
+    #[serde_inline_default(2)]
+    pub max_body_size: usize,
 }
 
 fn generate_connection_setting() -> ConnectionConfig {
     ConnectionConfig {
-        server_uri: "0.0.0.0:7890".to_string(),
+        server_addr: "0.0.0.0:7890".to_string(),
         db_uri: "postgresql://root:root@127.0.0.1:5432/toyou".to_string(),
+        tls: false,
+        ssl_cert: "./cert.crt".to_string(),
+        ssl_key: "./private.key".to_string(),
+        max_body_size: 2,
     }
 }
 
@@ -53,5 +70,20 @@ impl Config {
             .open("config.toml")
             .expect("Cannot open 'config.toml'");
         file.write_all(config_str.as_bytes()).unwrap();
+    }
+}
+
+pub async fn rename_log(now: DateTime<Local>) {
+    fs::create_dir_all("logs").await.unwrap();
+    let file_name = format!("logs/{}-least.log", now.format("%Y-%m-%d"));
+    if fs::try_exists(file_name.clone()).await.unwrap() {
+        let mut new_name = file_name.clone();
+        let mut file_name_offset = 0;
+        while fs::try_exists(new_name.clone()).await.unwrap() {
+            file_name_offset += 1;
+            new_name = format!("logs/{}-{file_name_offset}.log", now.format("%Y-%m-%d"));
+        }
+
+        fs::rename(file_name.clone(), new_name).await.unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,54 @@
-use crate::config::Config;
-use migration::{Migrator, MigratorTrait};
+use axum::{http, Router};
+use axum::body::Body;
+use axum::extract::DefaultBodyLimit;
+use axum::http::Request;
+use axum::routing::get;
+use axum_server::tls_rustls::RustlsConfig;
+use chrono::Local;
 use sea_orm::{ConnectOptions, Database};
+use tower_http::catch_panic::CatchPanicLayer;
+use tower_http::trace::TraceLayer;
+use tracing::{debug, info, Span, warn};
 use tracing::log::LevelFilter;
+use tracing_appender::non_blocking;
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use tracing_subscriber::{EnvFilter, fmt, Registry};
+use tracing_subscriber::fmt::time::ChronoLocal;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+use migration::{Migrator, MigratorTrait};
+
+use crate::config::{Config, rename_log};
 
 mod config;
 mod model;
 
-fn init_log() -> () {
-    let init = tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .try_init();
-    if init.is_err() {
-        tracing::warn!("Failed to initialize logger");
-    }
-}
-
 #[tokio::main]
 async fn main() {
     let config = Config::new();
-    init_log();
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(&config.trace_level));
+    let file_appender = RollingFileAppender::builder()
+        .rotation(Rotation::NEVER)
+        .filename_suffix(format!("logs/{}-least.log", Local::now().format("%Y-%m-%d")))
+        .build("").unwrap();
+    let (non_blocking_appender, _guard) = non_blocking(file_appender);
+
+    rename_log(Local::now()).await;
+    let formatting_layer = fmt::layer()
+        .with_writer(std::io::stderr)
+        .with_timer(ChronoLocal::new("%Y-%m-%d %H:%M:%S%.f(%:z)".to_string()));
+    let file_layer = fmt::layer()
+        .with_timer(ChronoLocal::new("%Y-%m-%d %H:%M:%S%.f(%:z)".to_string()))
+        .with_ansi(false)
+        .with_writer(non_blocking_appender);
+    Registry::default()
+        .with(env_filter)
+        .with(formatting_layer)
+        .with(file_layer)
+        .init();
+
 
     let mut opt = ConnectOptions::new(config.connection.db_uri);
     opt.sqlx_logging(true);
@@ -26,4 +56,34 @@ async fn main() {
     let db = Database::connect(opt).await.unwrap();
 
     Migrator::up(&db, None).await.unwrap();
+
+    let trace_layer = TraceLayer::new_for_http()
+        .on_request(|request: &Request<Body>, _span: &Span| {
+            debug!("{} {}", request.method(), request.uri().path());
+        })
+        .on_response(|response: &http::Response<Body>, _latency: std::time::Duration, _span: &Span| {
+            debug!("{}", response.status());
+        });
+
+    let app = Router::new()
+        .route("/ping", get(ping))
+        .layer(trace_layer)
+        .layer(CatchPanicLayer::new())
+        .layer(DefaultBodyLimit::max(config.connection.max_body_size * 1024 * 1024));
+    let addr = config.connection.server_addr.parse().unwrap();
+    info!("Listening: {addr}");
+
+    if config.connection.tls {
+        debug!("HTTPS enabled.");
+        let tls_config = RustlsConfig::from_pem_file(config.connection.ssl_cert, config.connection.ssl_key).await.unwrap();
+        axum_server::bind_rustls(addr, tls_config).serve(app.into_make_service()).await.unwrap();
+    } else {
+        warn!("HTTPS disabled.");
+        axum_server::bind(addr).serve(app.into_make_service()).await.unwrap();
+    }
+    println!("Hello, world!");
+}
+
+async fn ping() -> String {
+    "{\"version\" : \"0.1.0\"}".to_string()
 }
