c7104be41c0299a1c0fa8b2aa319d30f59f2b63f
zrll_ <zhengruilin0@126.com>
2024-03-21 17:00:01 +0800

    fix: wechat pay

--- src/service/trade/wechat/recall.rs
+++ src/service/trade/wechat/recall.rs
@@ -1,10 +1,13 @@
 use axum::http::HeaderMap;
-use axum::Json;
 use base64::Engine;
+use rsa::{Pkcs1v15Sign, RsaPublicKey};
+use rsa::pkcs8::DecodePublicKey;
+use rsa::signature::digest::Digest;
 use sea_orm::{ActiveModelTrait, EntityTrait, IntoActiveModel};
 use sea_orm::ActiveValue::Set;
 use serde::Serialize;
-use tracing::{debug, error, info};
+use sha2::Sha256;
+use tracing::{debug, info};
 use wechat_pay_rust_sdk::model::WechatPayNotify;
 use wechat_pay_rust_sdk::pay::PayNotifyTrait;
 use wechat_pay_rust_sdk::response::Certificate;
@@ -12,9 +15,9 @@ use x509_parser::parse_x509_certificate;
 use x509_parser::pem::parse_x509_pem;
 
 use crate::model::prelude::Trade;
-use crate::service::trade::{TradeStatus, WECHAT_PAY_CLIENT, WECHAT_PUBLIC};
+use crate::service::trade::{TradeStatus, WECHAT_PAY_CLIENT};
 
-pub async fn wechat_pay_recall(header_map: HeaderMap, body: String) -> Json<WechatNoticeResponse> {
+pub async fn wechat_pay_recall(header_map: HeaderMap, body: String) -> Result<String, String> {
     let request: WechatPayNotify = serde_json::from_str(&body).unwrap();
 
     //verify trade status
@@ -24,37 +27,24 @@ pub async fn wechat_pay_recall(header_map: HeaderMap, body: String) -> Json<Wech
     let result = WECHAT_PAY_CLIENT.decrypt_paydata(&chiphertext, &nonce, &associated_data).unwrap();
     let id = result.out_trade_no.clone();
     info!("Wechat pay recall: {result:?}");
-    
+
     let pub_key = get_public_key().await;
     let timestamp = header_map.get("Wechatpay-Timestamp").unwrap().to_str().unwrap();
     let nonce = header_map.get("Wechatpay-Nonce").unwrap().to_str().unwrap();
     let signature = header_map.get("Wechatpay-Signature").unwrap().to_str().unwrap();
     let body = body.as_str();
-    debug!("Wechat pay recall: {pub_key:?} {timestamp:?} {nonce:?} {signature:?} {body:?}");
+    debug!("Wechat pay recall: '{pub_key:?}' '{timestamp}' '{nonce}' '{signature}' '{body}'");
 
     //verify signature
-    if WECHAT_PAY_CLIENT.verify_signatrue(
-        &pub_key,
-        &timestamp,
-        &nonce,
-        &signature,
-        &body,
-    ).map_err(|e| {error!("{}", e.to_string())}).is_err() {
-        return Json(WechatNoticeResponse {
-            code: "FAIL".to_string(),
-            message: "签名错误".to_string(),
-        });
-    }
+    verify_signature(&pub_key, timestamp, nonce, signature, body)
+        .map_err(|e| format!(r#"{{"code": "FAIL", "message": "{}"}}"#, e.to_string())).unwrap();
 
     let mut trade = Trade::find_by_id(id).one(&*crate::DATABASE).await.unwrap().unwrap().into_active_model();
     trade.status = Set(TradeStatus::Paid as i16);
     trade.pay_time = Set(Some(chrono::Local::now().naive_local()));
     trade.save(&*crate::DATABASE).await.unwrap();
 
-    Json(WechatNoticeResponse {
-        code: "SUCCESS".to_string(),
-        message: "成功".to_string(),
-    })
+    Ok("".to_string())
 }
 
 #[derive(Serialize, Clone)]
@@ -66,10 +56,10 @@ pub struct WechatNoticeResponse {
 #[test]
 fn test_cert() {
     let pem = tokio::runtime::Runtime::new().unwrap().block_on(get_public_key());
-    println!("{pem}");
+    println!("{pem:?}");
 }
 
-async fn get_public_key() -> String {
+async fn get_public_key() -> Vec<u8> {
     //get cert from WeChat
     let cert = WECHAT_PAY_CLIENT.certificates().await.unwrap();
     let data: Certificate = cert.data.unwrap()[0].clone();
@@ -77,11 +67,29 @@ async fn get_public_key() -> String {
     let nonce = data.encrypt_certificate.nonce;
     let associated_data = data.encrypt_certificate.associated_data;
     let cert = WECHAT_PAY_CLIENT.decrypt_bytes(ciphertext, nonce, associated_data).unwrap();
-    
+
     //extract public key
     let res = parse_x509_pem(&cert).unwrap();
     let res_x509 = parse_x509_certificate(&res.1.contents).unwrap().1;
     let public_key = res_x509.public_key().raw;
-    let pem = base64::prelude::BASE64_STANDARD.encode(public_key);
-    format!("-----BEGIN PUBLIC KEY-----\n{}\n-----END PUBLIC KEY-----", pem)
+    
+    public_key.to_vec()
+}
+
+fn verify_signature(pub_key: &[u8], timestamp: &str, nonce: &str, signature: &str, body: &str) -> Result<(), String> {
+    let message = format!(
+        "{}\n{}\n{}\n",
+        timestamp,
+        nonce,
+        body
+    );
+    let pub_key = RsaPublicKey::from_public_key_der(pub_key)
+        .map_err(|e| e.to_string())?;
+    let hashed = Sha256::new().chain_update(message).finalize();
+    let signature = base64::prelude::BASE64_STANDARD.decode(signature.as_bytes())
+        .map_err(|e| e.to_string())?;
+    let scheme = Pkcs1v15Sign::new::<Sha256>();
+    pub_key
+        .verify(scheme, &hashed, signature.as_slice())
+        .map_err(|e| e.to_string())
 }
